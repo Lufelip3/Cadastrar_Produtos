93cd79bfe312ae0aaf0f5bb8ec5d8f19566befd9
Luis Felipe <luisfelipemathiasleite@gmail.com>
2025-10-03 21:40:28 -0300

    Criação do cadastro de produto
    Criação do cadastro de produto

---
+++ src/main/java/com/mycompany/cadastrar_produtos/Cadastrar_Produtos.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.cadastrar_produtos;
+
+/**
+ *
+ * @author luis.fmleite
+ */
+public class Cadastrar_Produtos {
+
+    private String nome;
+    private String descrição;
+    private int quantidade;
+    private double preco;
+
+    public Cadastrar_Produtos(String nome, String descrição, double preco, int quantidade) {
+        this.nome = nome;
+        this.descrição = descrição;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the descrição
+     */
+    public String getDescrição() {
+        return descrição;
+    }
+
+    /**
+     * @param descrição the descrição to set
+     */
+    public void setDescrição(String descrição) {
+        this.descrição = descrição;
+    }
+
+    /**
+     * @return the quantidade
+     */
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    /**
+     * @return the preco
+     */
+    public double getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+}
